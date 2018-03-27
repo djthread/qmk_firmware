df61995c24ed332b25ded9fac6a8cd9929ad2269
Adam Bellinson <adambellinson@quickenloans.com>
2018-03-27 14:54:18 -0400

    keyswap

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-make tv44:djthread && \
+make tv44:djthread44 && \
   sudo dfu-programmer atmega32u4 erase && \
-  sudo dfu-programmer atmega32u4 flash tv44_djthread.hex && \
+  sudo dfu-programmer atmega32u4 flash tv44_djthread44.hex && \
   sudo dfu-programmer atmega32u4 start
