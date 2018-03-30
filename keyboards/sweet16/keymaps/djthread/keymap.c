7dcf07eb8aefbe79268483ce2e622e249cb55e94
Adam Bellinson <adambellinson@quickenloans.com>
2018-03-30 13:50:34 -0400

    s16 keymap changes

--- keyboards/sweet16/keymaps/djthread/keymap.c
+++ keyboards/sweet16/keymaps/djthread/keymap.c
@@ -6,36 +6,44 @@
 // };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    // 0: Base
     LAYOUT_ortho_4x4(
         M_WLEFT,     M_WRIGHT,    M_WMAX,      M_WNEXTMON,
-        KC_TRNS,     KC_TRNS,     M_ZOOM,      RESET,
-        M_BACK,      M_FWD,       M_MTOGG,     M_MNEXT,
-        M_TABPREV,   M_TABNEXT,   M_RSTEX,     MO(1)
+        KC_TRNS,     KC_TRNS,     M_ZOOM,      M_RSTEX,
+        M_MTOGG,     M_BACK,      M_FWD,       M_MNEXT,
+        MO(2),       M_TABPREV,   M_TABNEXT,   MO(1)
     ),
 
+    // 1: Alt
+    LAYOUT_ortho_4x4(
+        TO(3),       KC_TRNS,     KC_TRNS,     KC_TRNS,
+        KC_TRNS,     KC_TRNS,     KC_TRNS,     RESET,
+        M_WTL,       M_WTR,       KC_TRNS,     M_MPREV,
+        M_WBL,       M_WBR,       KC_TRNS,     KC_TRNS
+    ),
+
+    // 2: Alt 2
     LAYOUT_ortho_4x4(
         KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,
         KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,
-        M_WTL,       M_WTR,       KC_TRNS,     M_MPREV,
-        M_WBL,       M_WBR,       KC_TRNS,     KC_TRNS
+        KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,
+        KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS
+    ),
+
+    // 3: Nav
+    LAYOUT_ortho_4x4(
+        KC_TRNS,      KC_PGUP,     KC_UP,       KC_PGDN,
+        KC_TRNS,      KC_LEFT,     KC_DOWN,     KC_RGHT,
+        KC_TRNS,      KC_TRNS,     KC_TRNS,     KC_TRNS,
+        MO(4),        KC_TRNS,     KC_TRNS,     TO(0)
+    ),
+
+    // 4: Nav Alt
+    LAYOUT_ortho_4x4(
+        KC_TRNS,      KC_HOME,     KC_UP,       KC_END,
+        KC_TRNS,      KC_LEFT,     KC_DOWN,     KC_RGHT,
+        KC_TRNS,      KC_TRNS,     KC_TRNS,     KC_TRNS,
+        KC_TRNS,      KC_TRNS,     KC_TRNS,     KC_TRNS
     )
+
 };
-
-/*
-void led_set_user(uint8_t usb_led) {
-
-  // Map RXLED to USB_LED_NUM_LOCK
-	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
-		DDRB |= (1 << 0); PORTB &= ~(1 << 0);
-	} else {
-		DDRB &= ~(1 << 0); PORTB &= ~(1 << 0);
-	}
-
-  // Map TXLED to USB_LED_CAPS_LOCK
-	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-		DDRD |= (1 << 5); PORTD &= ~(1 << 5);
-	} else {
-		DDRD &= ~(1 << 5); PORTD &= ~(1 << 5);
-	}
-}
-*/
