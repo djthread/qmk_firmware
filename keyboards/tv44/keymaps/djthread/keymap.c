8e640d69f01754028c904dee1706f8c645b4baec
thread <thethread@gmail.com>
2018-04-03 00:01:14 -0400

    things

--- keyboards/tv44/keymaps/djthread/keymap.c
+++ keyboards/tv44/keymaps/djthread/keymap.c
@@ -16,13 +16,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------`------`------`------`------`------`------`------`------`------`------`--------|
    * |   Shift   |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Sft/Ent|
    * |-----------`------`------`------`------`-----'-------`------`------`------`------`-------|
-   * |  Ctrl  |  Alt  |  Lower |  L2/Ent   |   Space     |  Cmd   | Ctl  |  L3   |
+   * |  Ctrl  |  Alt  |  Lower |  L2/Ent   |   Space     |  Cmd   |  L4  |  L3   |
    *  `-------+---------+--------+-----^^^------+-----^^^------+---------+------+------+-------' */
   KEYMAP(
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
     LT(1, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, LT(1, KC_QUOT),
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, SFT_T(KC_ENT),
-    KC_LCTL, KC_LALT, KC_LGUI, LT(2, KC_ENT), KC_SPC, KC_RGUI, KC_RCTL, MO(3)),
+    KC_LCTL, KC_LALT, KC_LGUI, LT(2, KC_ENT), KC_SPC, KC_RGUI, MO(4), MO(3)),
 
   /*  1: NUMBERS LAYER
    *
@@ -44,56 +44,73 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*  2: SHIFTED LAYER
    *
    * ,---------+------+------+------+------+------+------+------+------+------+------+---------.
-   * |   ~    |   !  |   @  |   #  |   $  |   %  |   ^  |   &  | PGUP |   (  |   )  |          |
+   * |   ~    |   !  |   @  |   #  |   $  |   %  |   ^  |   &  | PGUP |   (  |   )  |  DELETE  |
    * |---------`------`------`------`------`------`------`------`------`------`------`---------|
    * |        |   |  |   "  |   {  |   }  |   _  |   +  | HOME | PGDN |  END |   *  |          |
    * |----------`------`------`------`------`------`------`------`------`------`------`--------|
-   * |           |      |      |      |      |      |      |      |      |      |      |       |
+   * | ZOOM      |DESKT |EXPOSE| WLEFT|WRIGHT| WMAX|WNXMON|      |      |      |       |       |
    * |-----------`------`------`------`------`-----'-------`------`------`------`------`-------|
-   * |       |       |        |           |             |        |      |       |
+   * |         |        |       |           |                |          |           |          |
    *  `-------+---------+--------+-----^^^------+-----^^^------+---------+------+------+-------' */
   KEYMAP(
     KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_PGUP, KC_LPRN, KC_RPRN, KC_DEL,
     KC_TRNS, KC_PIPE, KC_DQUO, KC_LCBR, KC_RCBR, KC_UNDS, KC_PLUS, KC_HOME, KC_PGDN, KC_END, KC_ASTR, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    M_ZOOM, M_DESKTOP, M_EXPOSE, M_WLEFT, M_WRIGHT, M_WNEXTMON, KC_TRNS, KC_TRNS, KC_TRNS, BL_BRTG, BL_STEP, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
   /*  3: BONUS LAYER
-   * ,---------+------+------+------+------+------+------+------+------+------+------+---------.
-   * | CMD-`   |      |MBTN2 | MUP  | MBTN1 |MWHUP |      |      | WTL  | WTR  | DELETE  |
+   * ,---------+------+-----+------+------+------+------+------+------+------+------+---------.
+   * | CMD-`   |      |MBTN2 | MUP  | MBTN1 |MWHUP |      |      | WTL  | WTR |                |
    * |---------`------`------`------`------`------`------`------`------`------`------`---------|
-   * |TODVORAK |MBTN3 |MLEFT|MDOWN|MRIGHT|MWHDN |      |      | WBL  | WBR  |  BUILDINST  |
+   * |TODVORAK |MBTN3 |MLEFT |MDOWN |MRIGHT|MWHDN |      |       |      | WBL  | WBR  |BUILDINST|
    * |----------`------`------`------`------`------`------`------`------`------`------`--------|
-   * | ZOOM      |DESKT |EXPOSE| WLEFT|WRIGHT| WMAX|WNXMON|     |      |      |      |       |
+   * |           |      |      |      |      |      |      |      |      |      |      |       |
    * |-----------`------`------`------`------`-----'-------`------`------`------`------`-------|
-   * |TABPREV|TABNEXT|M_MPREV| M_MTOGG  |    M_MNEXT   |        |      |       |
+   * |  TABPREV   |TABNEXT|M_MPREV|  M_MTOGG    |    M_MNEXT   |        |         |            |
    *  `-------+---------+--------+-----^^^------+-----^^^------+---------+------+------+-------' */
   KEYMAP(
     M_CMDBT, KC_TRNS, KC_MS_BTN2, KC_MS_UP, KC_MS_BTN1, KC_MS_WH_UP, KC_TRNS, KC_TRNS, KC_TRNS, M_WTL, M_WTR, KC_TRNS,
-    TO(4), KC_MS_BTN3, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_WH_DOWN, KC_TRNS, KC_TRNS, KC_TRNS, M_WBL, M_WBR, M_BUILDINST,
-    M_ZOOM, M_DESKTOP, M_EXPOSE, M_WLEFT, M_WRIGHT, M_WNEXTMON, KC_TRNS, KC_TRNS, KC_TRNS, BL_BRTG, BL_STEP, KC_TRNS,
+    TO(5), KC_MS_BTN3, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_WH_DOWN, KC_TRNS, KC_TRNS, KC_TRNS, M_WBL, M_WBR, M_BUILDINST,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     M_TABPREV, M_TABNEXT, M_MPREV, M_MTOGG, M_MNEXT, KC_TRNS, KC_TRNS, KC_TRNS),
 
+  /*  4: MACRO LAYER
+   *
+   * ,---------+------+------+------+------+------+------+------+------+------+------+---------.
+   * |         |     |      |      |       |      |      |      |      |      |      |         |
+   * |---------`------`------`------`------`------`------`------`------`------`------`---------|
+   * |         |      |       |     |      |      |      |      |      |      |      |         |
+   * |----------`------`------`------`------`------`------`------`------`------`------`--------|
+   * |           |      |      |      |      |      |      |      |      |      |      |       |
+   * |-----------`------`------`------`------`-----'-------`------`------`------`------`-------|
+   * |   BACK     |  FWD   |       |             |              |        |  RSTEX  |           |
+   *  `-------+---------+--------+-----^^^------+-----^^^------+---------+------+------+-------' */
+  KEYMAP(
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    M_BACK, M_FWD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, M_RSTEX),
 
 
 
-  /*  4: DVORAK
+
+  /*  5: DVORAK
    * ,---------+------+------+------+------+------+------+------+------+------+------+---------.
    * |   Tab   |   '  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  |  Bksp   |
    * |---------`------`------`------`------`------`------`------`------`------`------`---------|
-   * | Ctrl/Esc |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |   -    |
+   * |  L6/Esc  |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |  L6/-  |
    * |----------`------`------`------`------`------`------`------`------`------`------`--------|
    * | Shift     |   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Sft/Ent|
    * |-----------`------`------`------`------`-----'-------`------`------`------`------`-------|
-   * | Ctrl | Alt  | Gui  |  Space    |    Space     |   Gui   | Ctrl  | L7  |
+   * | Ctrl | Alt  | Gui  |  Space    |    Space     |   Gui   |  L9   |   L8    |
    *  `-------+---------+--------+-----^^^------+-----^^^------+---------+------+------+-------' */
   KEYMAP(
     KC_TAB, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_BSPC,
-    LT(5, KC_ESC), KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, LT(5, KC_MINS),
+    LT(6, KC_ESC), KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, LT(6, KC_MINS),
     KC_LSFT, KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, SFT_T(KC_ENT),
-    KC_LCTRL, KC_LALT, KC_LGUI, MO(6), KC_SPC, KC_RGUI, KC_RCTL, MO(7)),
+    KC_LCTRL, KC_LALT, KC_LGUI, LT(7, KC_ENT), KC_SPC, KC_RGUI, MO(9), MO(8)),
 
-  /*  5: DVORAK NUMBERS LAYER
+  /*  6: DVORAK NUMBERS LAYER
    *
    * ,---------+------+------+------+------+------+------+------+------+------+------+---------.
    * |   `    |   1  |   2  |   3  |   4  |   5  |   6  |   7  |  UP  |   9  |   0  |          |
@@ -110,24 +127,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
-  /*  6: DVORAK SHIFTED LAYER
+  /*  7: DVORAK SHIFTED LAYER
    *
    * ,---------+------+------+------+------+------+------+------+------+------+------+---------.
    * |   ~    |   !  |   @  |   #  |   $  |   %  |   ^  |   &  | PGUP |   (  |   )  |          |
    * |---------`------`------`------`------`------`------`------`------`------`------`---------|
    * |        |   |  |   _  |   ?  |   +  |   {  |   }  | HOME | PGDN |  END  |   *  |          |
    * |----------`------`------`------`------`------`------`------`------`------`------`--------|
-   * |           |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  |       |
+   * | ZOOM      |DESKT |EXPOSE| WLEFT|WRIGHT| WMAX|WNXMON|      |      |      |       |       |
    * |-----------`------`------`------`------`-----'-------`------`------`------`------`-------|
    * |        |       |        |           |             |        |      |       |
    *  `-------+---------+--------+-----^^^------+-----^^^------+---------+------+------+-------' */
   KEYMAP(
     KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_PGUP, KC_LPRN, KC_RPRN, KC_TRNS,
     KC_TRNS, KC_PIPE, KC_UNDS, KC_QUES, KC_PLUS, KC_LCBR, KC_RCBR, KC_LEFT, KC_DOWN, KC_RGHT, KC_ASTR, KC_TRNS,
-    KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_TRNS,
+    M_ZOOM, M_DESKTOP, M_EXPOSE, M_WLEFT, M_WRIGHT, M_WNEXTMON, KC_TRNS, KC_TRNS, KC_TRNS, BL_BRTG, BL_STEP, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
-  /*  7: BONUS LAYER
+  /*  8: DVORAK BONUS LAYER
    *
    * ,---------+------+------+------+------+------+------+------+------+------+------+---------.
    * | CMD-`   |     | MBTN2| MUP  | MBTN1 |MWHUP |      |      |      |      |LEDST | DELETE  |
@@ -143,6 +160,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_MS_BTN3, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_WH_DOWN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, BL_BRTG, TO(0),
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+  /*  9: DVORAK MACRO LAYER
+   *
+   * ,---------+------+------+------+------+------+------+------+------+------+------+---------.
+   * |         |     |      |      |       |      |      |      |      |      |      |         |
+   * |---------`------`------`------`------`------`------`------`------`------`------`---------|
+   * |         |      |       |     |      |      |      |      |      |      |      |         |
+   * |----------`------`------`------`------`------`------`------`------`------`------`--------|
+   * |           |      |      |      |      |      |      |      |      |      |      |       |
+   * |-----------`------`------`------`------`-----'-------`------`------`------`------`-------|
+   * |   BACK     |  FWD   |       |             |              |        |  RSTEX  |           |
+   *  `-------+---------+--------+-----^^^------+-----^^^------+---------+------+------+-------' */
+  KEYMAP(
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    M_BACK, M_FWD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, M_RSTEX),
+
 };
 
 bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
