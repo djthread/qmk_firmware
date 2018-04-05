4d03200ac61c0aeacd5c93e36622f9f8cbd76397
Adam Bellinson <adambellinson@quickenloans.com>
2018-04-05 10:14:15 -0400

    teams: fml

--- keyboards/sweet16/keymaps/djthread/keymap.c
+++ keyboards/sweet16/keymaps/djthread/keymap.c
@@ -1,9 +1,9 @@
 #include "sweet16.h"
 #include "djthread.h"
 
-// enum custom_keycodes {
-//   M_SOMETHING = NEW_SAFE_RANGE,
-// };
+enum custom_keycodes2 {
+  M_BUILDINST = NEW_SAFE_RANGE,
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // 0: Base
@@ -16,8 +16,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // 1: Alt
     LAYOUT_ortho_4x4(
-        TO(3),       KC_TRNS,     KC_TRNS,     KC_TRNS,
-        KC_TRNS,     KC_TRNS,     KC_TRNS,     RESET,
+        TO(3),       KC_TRNS,     KC_TRNS,     TO(7),
+        KC_TRNS,     KC_TRNS,     KC_TRNS,     M_BUILDINST,
         M_WTL,       M_WTR,       KC_TRNS,     M_MPREV,
         M_WBL,       M_WBR,       KC_TRNS,     KC_TRNS
     ),
@@ -32,18 +32,56 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // 3: Nav
     LAYOUT_ortho_4x4(
-        KC_TRNS,      KC_PGUP,     KC_UP,       KC_PGDN,
+        TO(0),        KC_PGUP,     KC_UP,       KC_PGDN,
         KC_TRNS,      KC_LEFT,     KC_DOWN,     KC_RGHT,
         KC_TRNS,      KC_TRNS,     KC_TRNS,     KC_TRNS,
-        MO(4),        KC_TRNS,     KC_TRNS,     TO(0)
+        MO(4),        KC_TRNS,     KC_TRNS,     KC_TRNS
     ),
 
     // 4: Nav Alt
     LAYOUT_ortho_4x4(
-        KC_TRNS,      KC_HOME,     KC_UP,       KC_END,
+        TO(0),        KC_HOME,     KC_UP,       KC_END,
         KC_TRNS,      KC_LEFT,     KC_DOWN,     KC_RGHT,
         KC_TRNS,      KC_TRNS,     KC_TRNS,     KC_TRNS,
         KC_TRNS,      KC_TRNS,     KC_TRNS,     KC_TRNS
-    )
+    ),
+
+    // 5: TBD
+    LAYOUT_ortho_4x4(
+        TO(0),       KC_TRNS,     KC_TRNS,     KC_TRNS,
+        KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,
+        KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,
+        KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS
+    ),
+
+    // 6: TBD
+    LAYOUT_ortho_4x4(
+        TO(0),       KC_TRNS,     KC_TRNS,     KC_TRNS,
+        KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,
+        KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,
+        KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS
+    ),
+
+    // 7: TEAMS
+    LAYOUT_ortho_4x4(
+        TO(0),       KC_TRNS,     KC_TRNS,     KC_TRNS,
+        KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,
+        KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,
+        KC_A,        KC_TRNS,     M_TTREE,     M_TMSG
+    ),
 
 };
+
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    switch (keycode) {
+      case M_BUILDINST:
+        SEND_STRING("mavd ;,ddk16ZhckjodahZa.ohfhd" SS_TAP(X_ENTER));
+        _delay_ms(2000);
+        reset_keyboard();
+        return false;
+    }
+  }
+
+  return true;
+}
