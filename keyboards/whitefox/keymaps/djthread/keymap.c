f9e3be1c980cd8482f90b4e3455475d9be70aa75
thread <thethread@gmail.com>
2018-04-06 19:43:11 -0400

    first stab at whitefox keymap

--- keyboards/whitefox/keymaps/djthread/keymap.c
+++ keyboards/whitefox/keymaps/djthread/keymap.c
@@ -15,37 +15,92 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "whitefox.h"
+#include "djthread.h"
+
+enum custom_keycodes2 {
+  M_BUILDINST = NEW_SAFE_RANGE
+};
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer 0: Default Layer
      * ,---------------------------------------------------------------.
-     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|  `|Ins|
+     * | ` |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|   |   |
      * |---------------------------------------------------------------|
-     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|Del|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|Zoo|
      * |---------------------------------------------------------------|
-     * |CapsLo|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |PgU|
+     * |L1/Esc|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|L1/Enter|PgU|
      * |---------------------------------------------------------------|
-     * |Shif|   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Arr|PgD|
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|PgD|
      * |---------------------------------------------------------------|
-     * |Ctrl|Alt |Gui |         Space    |Fn0 |Alt |Gui |  |Fn1|Fn2|Fn3|
+     * |Ctrl|Gui |Alt |         Space         |Gui |L2  |  |Lef|Dow|Rig|
      * `---------------------------------------------------------------'
      */
-    [0] = KEYMAP( \
-        KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS,KC_GRV, KC_INS, \
-        KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSPC,     KC_DEL, \
-        KC_CAPS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_NUHS,KC_ENT,      KC_PGUP,\
-        KC_LSFT,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,     KC_UP,  KC_PGDN,\
-        KC_LCTL,KC_LALT,KC_LGUI,               KC_SPC,           KC_RALT,KC_FN0, KC_RCTL,     KC_LEFT,KC_DOWN,KC_RGHT \
+    [0] = KEYMAP_TRUEFOX( \
+        KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS,KC_TRNS, KC_TRNS, \
+        KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSPC,     M_ZOOM, \
+    LT(1, KC_ESC),KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,LT(1, KC_ENT),      KC_PGUP, \
+        KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,     KC_UP,  KC_PGDN, \
+        KC_LCTL,KC_LALT,KC_LGUI,               KC_SPC,           KC_RGUI,MO(2),                              KC_LEFT,KC_DOWN,KC_RGHT \
     ),
-    [1] = KEYMAP( \
-        KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_TRNS,KC_TRNS,KC_MUTE,\
-        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_PSCR,KC_SLCK,KC_PAUS,KC_TRNS,     KC_TRNS,\
-        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_VOLU,\
-        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_PGUP,KC_VOLD,\
-        KC_TRNS,KC_TRNS,KC_TRNS,               KC_TRNS,          KC_TRNS,KC_TRNS,KC_TRNS,     KC_HOME,KC_PGDN,KC_END  \
+
+    /* Layer 1
+     * ,---------------------------------------------------------------.
+     * |ClN| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   |Br-|Br+|
+     * |---------------------------------------------------------------|
+     * |     |   |Bn2|MUp|Bn1|MWU|   |   | Up|   |   |   |   |  Del|Mut|
+     * |---------------------------------------------------------------|
+     * |      |Bn3|MLe|MDn|MRi|MWD|   |Lef|Dow|Rig|MuT|MuN|        |VoU|
+     * |---------------------------------------------------------------|
+     * |        |Des|Exp|WLe|WRi|WMx|WNM|   |   |   |   |      |PgU|VoD|
+     * |---------------------------------------------------------------|
+     * |TPre|TNex|    |                       |    |    |  |Hom|PgD|End|
+     * `---------------------------------------------------------------'
+     */
+    [1] = KEYMAP_TRUEFOX( \
+        M_DISMISSNOTI,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_TRNS,KC_F14,KC_F15,\
+        KC_TRNS,KC_TRNS,KC_MS_BTN2,KC_MS_UP,KC_MS_BTN1,KC_MS_WH_UP,KC_TRNS,KC_TRNS,KC_UP,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_DEL,     KC_MUTE,\
+        KC_TRNS,KC_MS_BTN3,KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT,KC_MS_WH_DOWN,KC_TRNS,KC_LEFT,KC_DOWN,KC_RGHT,M_MTOGG,M_MNEXT,KC_TRNS,     KC_VOLU,\
+        KC_TRNS,M_DESKTOP,M_EXPOSE,M_WLEFT,M_WRIGHT,M_WMAX,M_WNEXTMON,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_PGUP,KC_VOLD,\
+        M_TABPREV,M_TABNEXT,KC_TRNS,               KC_TRNS,          KC_TRNS,KC_TRNS,     KC_HOME,KC_PGDN,KC_END  \
+    ),
+
+    /* Layer 2
+     * ,---------------------------------------------------------------.
+     * |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+     * |---------------------------------------------------------------|
+     * |     |   |   |   |   |   |   |   |   |   |   |WTL|WTR|     |   |
+     * |---------------------------------------------------------------|
+     * |      |   |   |   |   |   |   |   |   |   |WBL|WBR|        |   |
+     * |---------------------------------------------------------------|
+     * |        |   |   |   |   |   |   |   |   |Sea|New| Lock |   |   |
+     * |---------------------------------------------------------------|
+     * |    |    |    |          Back         | Fwd|    |  |   |   |   |
+     * `---------------------------------------------------------------'
+     */
+    [1] = KEYMAP_TRUEFOX( \
+        KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,\
+        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,M_WTL,M_WTR,KC_TRNS,     KC_TRNS,\
+        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,M_WBL,M_WBR,KC_TRNS,     KC_TRNS,\
+        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,M_SEARCHNOTES,M_NEWNOTE,M_LOCK,     KC_TRNS,KC_TRNS,\
+        KC_TRNS,KC_TRNS,KC_TRNS,               M_BACK,          M_FWD,KC_TRNS,     KC_TRNS,KC_TRNS,KC_TRNS  \
     ),
 };
 
+/*
 const uint16_t fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),
 };
+*/
+
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    switch (keycode) {
+      case M_BUILDINST:
+        SEND_STRING("mav k.44ZhckjodahZhyf" SS_TAP(X_ENTER));
+        reset_keyboard();
+        return false;
+    }
+  }
+
+  return true;
+}
