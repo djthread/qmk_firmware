15087a90c45ea97d56ad0ba0bbec65e64e22bcce
Adam Bellinson <adambellinson@quickenloans.com>
2018-04-11 11:19:04 -0400

    beginning work on nightfox

--- keyboards/whitefox/keymaps/djthread/keymap.c
+++ keyboards/whitefox/keymaps/djthread/keymap.c
@@ -17,19 +17,25 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "whitefox.h"
 #include "djthread.h"
 
-enum custom_keycodes2 {
+enum nightfox_layers {
+  _QWERTY,
+  _NAVIGATION,
+  _FUNCTION
+}
+
+enum nightfox_keycodes {
   M_BUILDINST = NEW_SAFE_RANGE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    /* Layer 0: Default Layer
+    /* QWERTY LAYER
      * ,---------------------------------------------------------------.
      * | ` |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|Zoo|Hom|
      * |---------------------------------------------------------------|
      * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|End|
      * |---------------------------------------------------------------|
-     * |L1/Esc|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|L1/Enter|PgU|
+     * |Nv/Esc|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '| Nav/Ent|PgU|
      * |---------------------------------------------------------------|
      * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|PgD|
      * |---------------------------------------------------------------|
@@ -39,14 +45,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP_TRUEFOX( \
         KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS,M_ZOOM, KC_HOME, \
         KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSPC,     KC_END, \
-    LT(1, KC_ESC),KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,LT(1, KC_ENT),      KC_PGUP, \
+    LT(_NAVIGATION, KC_ESC),KC_A,  KC_S,  KC_D,  KC_F,  KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,LT(_NAVIGATION, KC_ENT),KC_PGUP, \
         KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,     KC_UP,  KC_PGDN, \
-        KC_LCTL,KC_LALT,KC_LGUI,               KC_SPC,           KC_RGUI,MO(2),                              KC_LEFT,KC_DOWN,KC_RGHT \
+        KC_LCTL,KC_LALT,KC_LGUI,               KC_SPC,           KC_RGUI,MO(_FUNCTION),                              KC_LEFT,KC_DOWN,KC_RGHT \
     ),
 
-    /* Layer 1
+    /* NAVIGATION LAYER
      * ,---------------------------------------------------------------.
-     * |ClN| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   |Br-|Br+|
+     * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   |Br-|Br+|
      * |---------------------------------------------------------------|
      * |     |   |Bn2|MUp|Bn1|MWU|PgU|Hom| Up|End|FoV|   |   |  Del|Mut|
      * |---------------------------------------------------------------|
@@ -58,16 +64,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `---------------------------------------------------------------'
      */
     KEYMAP_TRUEFOX( \
-        M_DISMISSNOTI,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_TRNS,KC_F14,KC_F15,\
+        KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_TRNS,KC_F14,KC_F15,\
         KC_TRNS,KC_TRNS,KC_MS_BTN2,KC_MS_UP,KC_MS_BTN1,KC_MS_WH_UP,KC_PGUP,KC_HOME,KC_UP,KC_END,M_FOCUSVIEW,KC_TRNS,KC_TRNS,KC_DEL,     KC_MUTE,\
         KC_TRNS,KC_MS_BTN3,KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT,KC_MS_WH_DOWN,KC_PGDN,KC_LEFT,KC_DOWN,KC_RGHT,M_MTOGG,M_MNEXT,KC_TRNS,     KC_VOLU,\
         KC_TRNS,M_DESKTOP,M_EXPOSE,M_WLEFT,M_WRIGHT,M_WMAX,M_WNEXTMON,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_TRNS,KC_VOLD,\
         KC_TRNS,KC_TRNS,KC_TRNS,               KC_TRNS,          KC_TRNS,KC_TRNS,                         KC_TRNS,KC_TRNS,KC_TRNS  \
     ),
 
-    /* Layer 2
+    /* FUNCTION LAYER
      * ,---------------------------------------------------------------.
-     * |Res|   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+     * |ClN|   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
      * |---------------------------------------------------------------|
      * |     |   |   |   |   |   |   |   |   |   |   |WTL|WTR|RSTEX|   |
      * |---------------------------------------------------------------|
@@ -79,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `---------------------------------------------------------------'
      */
     KEYMAP_TRUEFOX( \
-        RESET,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,\
+        M_DISMISSNOTI,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,\
         KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,M_WTL,M_WTR,M_RSTEX,     KC_TRNS,\
         KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,M_WBL,M_WBR,KC_TRNS,     KC_TRNS,\
         KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,M_SEARCHNOTES,M_NEWNOTE,M_LOCK,     KC_TRNS,KC_TRNS,\
