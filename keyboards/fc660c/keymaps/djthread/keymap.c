25e6cc33f87e9b27dd2926dc4b714ec5b515f235
thread <thethread@gmail.com>
2018-05-02 20:17:00 -0400

    .

--- keyboards/fc660c/keymaps/djthread/keymap.c
+++ keyboards/fc660c/keymaps/djthread/keymap.c
@@ -37,22 +37,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,KC_LALT,KC_LGUI,                     KC_SPC,                       KC_RGUI,MO(_FUNCTION),MO(_FUNCTION2),      KC_LEFT,KC_DOWN,KC_RGHT
   ),
   [_NAVIGATION] = KEYMAP(
-        M_CMD_H, KC_F1,  KC_F2,  KC_F3,  M_CHATTERM,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, M_APPTERM, KC_F12, _______,     _______,
-        M_CMD_TAB,M_CMDBT,KC_MS_BTN2,KC_MS_UP,KC_MS_BTN1,KC_MS_WH_UP,KC_PGUP,KC_HOME,KC_UP,KC_END,M_FOCUSVIEW,M_MTOGG,M_MNEXT,_______,     KC_DEL,
+        M_CMD_H, KC_F1,  KC_F2,  KC_F3,  M_CHATTERM,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, M_APPTERM, KC_F12, _______,     KC_HOME,
+        M_CMD_TAB,M_CMDBT,KC_MS_BTN2,KC_MS_UP,KC_MS_BTN1,KC_MS_WH_UP,KC_PGUP,KC_HOME,KC_UP,KC_END,M_FOCUSVIEW,M_MTOGG,M_MNEXT,KC_DEL,     KC_END,
         _______,_______,KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT,KC_MS_WH_DOWN,KC_PGDN,KC_LEFT,KC_DOWN,KC_RIGHT,_______,M_ZOOM,     _______,
         _______,M_DESKTOP,M_EXPOSE,M_WLEFT,M_WRIGHT,M_WMAX,M_WNEXTMON,M_GOLEFT,M_GORIGHT,M_S_PGUP,M_S_PGDN,M_RSTEX,     M_FWD,
         _______,_______,_______,                           M_BACK,                M_FWD,M_TABPREV,M_TABNEXT,     M_TABPREV,M_BACK,M_TABNEXT
   ),
   [_FUNCTION] = KEYMAP(
-        M_DISMISSNOTI, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, M_LOCK,     _______,
-        _______,_______,_______,_______,_______,M_KEYBTERM,_______,_______,_______,_______,_______,_______,_______,_______,     _______,
+        M_DISMISSNOTI, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, M_LOCK,     KC_HOME,
+        _______,_______,_______,_______,_______,M_KEYBTERM,_______,_______,_______,_______,_______,_______,_______,_______,     KC_END,
         _______,_______,M_TEAMS,M_OUTLOOK,M_WTL,M_WTR,  M_WTOP,_______,_______,_______,M_NEWBROWSER,_______,     TO(_NAVIGATION),
         _______,M_TTREE,M_TMSG, _______,M_WBL,  M_WBR,  M_WBOTTOM,M_SEARCHNOTES,M_NEWNOTE,M_MTOGG,M_MNEXT,_______,     KC__VOLUP,
         _______,_______,_______,                         _______,                _______,_______,_______,     KC_F14,KC__VOLDOWN,KC_F15
   ),
   [_FUNCTION2] = KEYMAP(
-        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, KC_F17, KC_F19, M_LOCK,     _______,
-        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,M_BUILDINST,     _______,
+        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, KC_F17, KC_F19, M_LOCK,     KC_HOME,
+        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,M_BUILDINST,     KC_END,
         _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,     TO(_NAVIGATION),
         _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,     _______,
         _______,_______,_______,                         _______,                _______,_______,_______,     _______,_______,_______
