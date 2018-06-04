987930658e0332c4dcc9c25860d0fbb56042b9ca
thread <thethread@gmail.com>
2018-06-03 22:16:40 -0400

    .

--- keyboards/dz60/keymaps/djthread/keymap.c
+++ keyboards/dz60/keymaps/djthread/keymap.c
@@ -19,16 +19,6 @@ enum dz60_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  /*
-  [_MAIN] = LAYOUT_thread(
-        KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,KC_EQL, KC_BSLS, M_BUILDINST,
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC,KC_RBRC,KC_BSPC,
-        LT(_NAVIGATION, KC_ESC),KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN,KC_QUOT, LT(_NAVIGATION, KC_ENT),
-        KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_1, KC_2, KC_3,
-        KC_1, KC_1, KC_1,  KC_1, KC_1, KC_1,  KC_1, KC_1, KC_1,KC_1,KC_1
-  ),
-  */
-
   [_MAIN] = LAYOUT_thread(
     KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,KC_EQL, KC_BSLS, M_CMDTAB,
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC,KC_RBRC,KC_BSPC,
@@ -38,11 +28,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_NAVIGATION] = LAYOUT_thread(
-    M_CMD_W, KC_F1, KC_F2, KC_F3, M_CHATTERM, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, M_APPTERM, KC_F12, _______, M_CMD_H,
+    M_CMD_W, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, M_CMD_H,
     M_CMDTAB, M_CMDBT, KC_MS_BTN2, KC_MS_UP, KC_MS_BTN1, KC_MS_WH_UP, KC_PGUP, KC_HOME, KC_UP, KC_END, M_FOCUSVIEW, M_MTOGG, M_MNEXT, KC_DEL,
     _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_WH_DOWN, KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, M_ZOOM,
     _______, M_DESKTOP, M_EXPOSE, M_WLEFT, M_WRIGHT, M_WMAX, M_WNEXTMON, M_GOLEFT, M_GORIGHT, M_S_PGUP, M_S_PGDN, M_FWD, M_RSTEX,
-    _______, _______, _______,     _______, _______, M_BACK,     M_FWD, TO(_MAIN), _______, _______, _______
+    _______, _______, _______,     _______, _______, _______,     TO(_MAIN), M_BACK, _______, M_FWD, _______
   ),
 
   [_ARROWS] = LAYOUT_thread(
@@ -63,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_FN1] = LAYOUT_thread(
     M_DISMISSNOTI, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______, KC_HOME,
-    _______, _______, _______, _______, _______, M_KEYBTERM, _______, _______, _______, _______, _______, _______, _______, KC_END,
+    _______, _______, _______, M_CHATTERM, M_APPTERM, M_KEYBTERM, _______, _______, _______, _______, _______, M_MTOGG, M_MNEXT, KC_END,
     _______, _______, M_TEAMS, M_OUTLOOK, M_WTL, M_WTR,  M_WTOP, _______, _______, _______, M_NEWBROWSER, _______,     KC_PGUP,
     _______, M_TTREE, M_TMSG, _______, M_WBL,  M_WBR,  M_WBOTTOM, M_SEARCHNOTES, M_NEWNOTE, M_MTOGG, M_MNEXT,     KC_UP, KC_PGDN,
     _______, _______, _______,      _______, _______, _______,         _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT
@@ -89,8 +79,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_END,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, M_LBCKT2X, M_RBCKT2X,              KC_F,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     KC_V,     KC_H,
-    _______, _______, _______,      _______, _______, _______,         TO(_MAIN), _______, KC_LEFT, KC_C, KC_RIGHT
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME,     KC_V,     KC_END,
+    _______, _______, _______,      _______, _______, _______,         TO(_MAIN), _______, KC_F, KC_C, KC_H
   ),
 
   [_DVORAK] = LAYOUT_thread(
