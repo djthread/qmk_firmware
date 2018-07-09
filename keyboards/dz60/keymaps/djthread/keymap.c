02e2668d97975a57d9fb16a3c1bbdfdac3bec911
thread <thethread@gmail.com>
2018-07-08 21:39:22 -0400

    .

--- keyboards/dz60/keymaps/djthread/keymap.c
+++ keyboards/dz60/keymaps/djthread/keymap.c
@@ -35,13 +35,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     M_CMDTAB, M_CMDBT, KC_MS_BTN2, KC_MS_UP, KC_MS_BTN1, KC_MS_WH_UP, KC_PGUP, KC_HOME, KC_UP, KC_END, M_FOCUSVIEW, M_MTOGG, M_MNEXT, KC_DEL,
     _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_WH_DOWN, KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, _______, M_ZOOM, _______,
     _______, M_DESKTOP, M_EXPOSE, M_WLEFT, M_WRIGHT, M_WMAX, M_WNEXTMON, M_GOLEFT, M_GORIGHT, M_S_PGUP, M_S_PGDN, M_FWD, M_RSTEX,
-    _______, _______, _______,     _______, _______, _______,     TO(_MAIN), M_BACK, _______, M_FWD, _______
+    _______, _______, _______,     _______, _______, _______,     TO(_MAIN), _______, _______, _______, _______
   ),
 
   [_ALTLAYER] = LAYOUT_thread(
     M_DISMISSNOTI, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_F13, KC_HOME,
     M_CMDTAB, M_CMDBT, _______, _______, M_APPTERM, M_KEYBTERM, _______, _______, M_CHATTERM, _______, _______, M_MTOGG, M_MNEXT, KC_END,
-    M_BARTENDERSHOW, _______, M_TEAMS, M_OUTLOOK, M_WTL, M_WTR,  M_WTOP, _______, _______, _______, M_NEWBROWSER, _______,     KC_PGUP,
+    M_BARTENDERSHOW, _______, M_TEAMS, M_OUTLOOK, M_WTL, M_WTR,  M_WTOP, M_WLEFT, M_WRIGHT, M_WMAX, M_WNEXTMON, M_NEWBROWSER,     KC_PGUP,
     _______, M_TTREE, M_TMSG, _______, M_WBL,  M_WBR,  M_WBOTTOM, M_SEARCHNOTES, M_NEWNOTE, M_MTOGG, M_MNEXT,     KC_UP, KC_PGDN,
     _______, _______, _______,      _______, _______, _______,         _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT
   ),
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,              _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP,     KC_UP,     KC_PGDN,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     KC_UP,     KC_TAB,
     _______, _______, _______,      _______, _______, _______,         TO(_MAIN), MO(_ARROWS2), KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
@@ -65,9 +65,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_FN1] = LAYOUT_thread(
     M_DISMISSNOTI, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______, KC_HOME,
     _______, _______, _______, M_CHATTERM, M_APPTERM, M_KEYBTERM, _______, _______, _______, _______, _______, M_MTOGG, M_MNEXT, KC_END,
-    _______, _______, M_TEAMS, M_OUTLOOK, M_WTL, M_WTR,  M_WTOP, _______, _______, _______, M_NEWBROWSER, _______,     KC_PGUP,
+    _______, _______, M_TEAMS, M_OUTLOOK, M_WTL, M_WTR,  M_WTOP, _______, _______, _______, _______, M_NEWBROWSER,     KC_PGUP,
     _______, M_TTREE, M_TMSG, _______, M_WBL,  M_WBR,  M_WBOTTOM, M_SEARCHNOTES, M_NEWNOTE, M_MTOGG, M_MNEXT,     KC_UP, KC_PGDN,
-    _______, _______, _______,      _______, _______, _______,         _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT
+    _______, _______, _______,      _______, _______, M_BACK,         M_FWD, _______, KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
   [_SYS] = LAYOUT_thread(
